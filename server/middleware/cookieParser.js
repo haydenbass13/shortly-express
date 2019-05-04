64dc6107cf58b66cef77751cd19506eb823c9184
Adam Gipril <adam.gipril@icloud.com>
2019-05-03 18:23:45 -0600

    Complete through cookie parser, about to start on sessions
    Co-authored by Hayden Bass

--- server/middleware/cookieParser.js
+++ server/middleware/cookieParser.js
@@ -1,4 +1,12 @@
 const parseCookies = (req, res, next) => {
+  if (req.headers.cookie) {
+    let cookieArray = req.headers.cookie.split(';');
+    cookieArray.forEach(cookie => {
+      let temp = cookie.split('=');
+      req.cookies[temp[0].trim()] = temp[1];
+    });
+  }
+  next();
 };
 
 module.exports = parseCookies;
